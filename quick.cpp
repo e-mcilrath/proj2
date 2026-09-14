1dbf71ace1c43ebb9540a6d60d9991b2ef0055df
Eric McIlrath <edmcilrath@gmail.com>
2026-09-14 13:46:29 -0400

    quick.cpp

--- quick.cpp
+++ quick.cpp
@@ -10,20 +10,67 @@ Node *qsort(Node *head, bool numeric);
 void  partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric);
 Node *concatenate(Node *left, Node *right);
 
-// Implementations
-
-// walk list 
-// vector of Node* or array (no iterators needed)
-
 void quick_sort(List &l, bool numeric) {
+    l.head = qsort(l.head, numeric); //just a shell
 }
 
 Node *qsort(Node *head, bool numeric) {
+
+    if (head == nullptr || head->next == nullptr) { // check for 1 or 0 in list
+        return head;
+    }
+
+    Node *pivot = head;          // pivot is head 
+    Node *rest = head->next;     // dont lose the rest of the list 
+    pivot->next = nullptr;       //detach head
+
+    // initialize left and right
+    Node *left = nullptr;  
+    Node *right = nullptr;
+
+    partition(rest, pivot, left, right, numeric);
+
+    //sort left and right
+    left = qsort(left, numeric);
+    right = qsort(right, numeric);
+
+    // final product
+    return concatenate(concatenate(left, pivot), right);
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, bool numeric) {
+
+    // make sure they're empty
+    left = nullptr;
+    right = nullptr;
+
+    // run through the list
+    for (Node *current = head; current != nullptr; ) { // empty because using & and changing *->next
+
+        Node *next = current->next; //save *->next
+        if (numeric ? current->number < pivot->number : current->string < pivot->string) { // compare number or string
+            current->next = left;   // add current node to left
+            left = current;
+        }
+        else {
+            current->next = right;  //add current node to right
+            right = current;
+        }
+        current = next; // interate
+    }
 }
 
 Node *concatenate(Node *left, Node *right) {
-}
 
+    if (left == nullptr) {
+        return right;
+    }
+    else {
+        Node *temp = left;
+        while (temp->next != nullptr) { // get to the end of left
+            temp = temp->next;
+        }
+        temp->next = right; //concatenate
+        return left;
+    }
+}
