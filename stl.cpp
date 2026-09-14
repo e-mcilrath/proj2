ba3190ed3fd68099012d78c6aa45d9b735fe50ec
Abraham Rashdan <aarashda08@gmail.com>
2026-09-14 12:19:17 -0400

    Abe changes 1

--- stl.cpp
+++ stl.cpp
@@ -5,21 +5,31 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+using namespace std;
+// C++ Style comparison function
+bool node_number_compare(const Node *a, const Node *b){ //returns true if the first nodes number is less else returns false
+    if (a-> number < b-> number) return true; 
+    else return false;
 
-//compare number (1)
-//compare string (0)
+};
+
+bool node_string_compare(const Node *a, const Node *b){ //returns true if the first nodes string is less else returns false
+
+    if (a-> string < b-> string) return true; 
+    else return false;
+
+}
 
 void stl_sort(List &l, bool numeric) {
     std::vector<Node*> nodes;
 
-    for (Node* current = l.head; current != nullptr; current = current->next) {
+    for (Node* current = l.head; current != nullptr; current = current->next) { // iterate through the list and push it back into a vector
         nodes.push_back(current);
     }
 
 
-    std::sort(nodes.begin(), nodes.end(), [numeric](Node* a, Node* b) {
-        return a->number < b->number; // TODO int or string based on 'numeric'
-    }); 
+    if (numeric == true) sort(nodes.begin(), nodes.end(), node_string_compare);   
+    else sort(nodes.begin(), nodes.end(), node_number_compare);
 
     nodes[nodes.size() - 1]->next = nullptr;
     l.head = nodes.front();
