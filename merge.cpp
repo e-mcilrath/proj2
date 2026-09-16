f1253326a6e1c81d91db3dfc3ee04e186bf2a35d
Abraham Rashdan <aarashda08@gmail.com>
2026-09-15 21:49:15 -0400

    merge algorithim

--- merge.cpp
+++ merge.cpp
@@ -15,6 +15,10 @@ Node *merge(Node *left, Node *right, bool numeric);
 // pick int or string
 // 
 void merge_sort(List &l, bool numeric) {
+
+
+
+
 }
 
 Node *msort(Node *head, bool numeric) {
@@ -32,8 +36,100 @@ Node *msort(Node *head, bool numeric) {
 }
 
 void split(Node *head, Node *&left, Node *&right) {
+
+
 }
 
 Node *merge(Node *left, Node *right, bool numeric) {
+    Node *head, *tail;
+
+    
+    
+    if(numeric){ //if comparing numbers
+
+        if(left-> number <= right-> number){ 
+
+         head = left; // first we initialize the head of the list
+        left = left-> next; 
+        } 
+        else {
+            head = right; 
+            right = right-> next; 
+        }
+        
+        tail = head; // start from the beginning of the list this is our dummy tail
+        while(left!= nullptr && right != nullptr){
+
+            if(left-> number <= right-> number ) { 
+                tail->next = left; //push left to the list
+                tail = left; // move tail to point to that number
+                left = left-> next; //move to the next number in the left list
+
+            }
+            else { // repeat for right
+                tail-> next = right; 
+                tail = right; 
+                right = right -> next; 
+                
+                
+            }
+
+            
+
+
+        }
+        // these two check if there are any remaining numbers in either side
+        if(left != nullptr) tail-> next = left;
+        else if( right != nullptr) tail-> next = right;
+        
+
+
+
+    }
+
+    else { // do the same thing for merging strings
+        if(left-> string <= right-> string){ 
+
+         head = left; // first we initialize the head of the list
+        left = left-> next; 
+        } 
+        else {
+            head = right; 
+            right = right-> next; 
+        }
+        
+        tail = head; // start from the beginning of the list this is our dummy tail
+        while(left!= nullptr && right != nullptr){
+
+            if(left-> string <= right-> string ) { 
+                tail->next = left; //push left to the list
+                tail = left; // move tail to point to that number
+                left = left-> next; //move to the next number in the left list
+
+            }
+            else { // repeat for right
+                tail-> next = right; 
+                tail = right; 
+                right = right -> next; 
+                
+                
+            }
+
+            
+
+
+        }
+        // these two check if there are any remaining numbers in either side
+        if(left != nullptr) tail-> next = left;
+        else if( right != nullptr) tail-> next = right;
+
+    }
+
+
+
+
+   
+
+
 }
 
